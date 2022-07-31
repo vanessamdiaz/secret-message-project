4d668bef15e3dcd8b0717e7de885d400e4c2977e
vanessamdiaz <109388381+vanessamdiaz@users.noreply.github.com>
2022-07-31 12:05:27 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 
+'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 
+'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 
+'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+secretMessage.length;
+secretMessage.splice(24, 25, 'to', 'Program');
+secretMessage.splice(7, 1, 'right');
+secretMessage.shift();
+secretMessage.splice(0, 0, 'Programming');
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage);
